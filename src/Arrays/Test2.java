3c35ff6c93baf0ab230cabe60a6f653d81ef5c46
sudhirskp <gnitwallah@gmail.com>
2025-12-25 20:44:13 +0530

    add implementation for Mountain Array validation

---
+++ src/Arrays/Test2.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class Test2 {
+
+    boolean mountainArray (int [] arr){
+        int n = arr.length;
+        int i = 0;
+        while(i+1 < n && arr[i] < arr[i+1]) {
+            i++;
+        }
+        if(i == n-1) return true;
+        while(i+1 < n && arr[i] > arr[i+1]) {
+            i++;
+        }
+        return i == n-1;
+    }
+    public static void main(String[] args) {
+        Test2 ob = new Test2();
+        int [] arr = {1,2,3,4,5,2,1,0};
+        System.out.println(ob.mountainArray(arr));
+    }
+}
