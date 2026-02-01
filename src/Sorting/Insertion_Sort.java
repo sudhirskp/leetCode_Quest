0ae161c6ba9c7adada4553acbf318ac83fb1e264
sudhirskp <gnitwallah@gmail.com>
2026-02-01 11:13:58 +0530

    add implementation for Insertion_Sort class to perform insertion sort on a linked list

---
+++ src/Sorting/Insertion_Sort.java
@@ -0,0 +1,32 @@
+package Sorting;
+
+//make listNode for run
+public class Insertion_Sort {
+
+//    public ListNode insertionSortList(ListNode head) {
+//        if(head==null) return head;
+//        ListNode dm = new ListNode(0);
+//        ListNode curr = head;
+//
+//
+//        while (curr!= null) {
+//            ListNode prev = dm;
+//            ListNode next = curr.next;
+//            while (prev.next != null && prev.next.val < curr.val) {
+//                prev = prev.next;
+//            }
+//
+//            curr.next = prev.next;
+//            prev.next = curr;
+//
+//            curr = next;
+//
+//
+//        }
+//        return dm.next;
+//    }
+
+    public static void main(String[] args) {
+
+    }
+}
